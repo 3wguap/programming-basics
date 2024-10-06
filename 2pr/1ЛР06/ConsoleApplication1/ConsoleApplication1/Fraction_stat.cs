44837318d5afa0bc37e25faf1cf66af7cc7bce89
3wguap <rnbrell156@gmail.com>
2024-10-06 14:34:28 +0400

    Add files via upload

---
+++ 2pr/1ЛР06/ConsoleApplication1/ConsoleApplication1/Fraction_stat.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Fraction_stat
+    {
+        static public Fraction SumSt(Fraction frac1, Fraction frac2)
+        {
+            return new Fraction(frac1.Numerator * frac2.Denominator + frac1.Denominator * frac2.Numerator, frac1.Denominator * frac2.Denominator);
+        }
+        static public Fraction DifSt(Fraction frac1, Fraction frac2)
+        {
+            return new Fraction(frac1.Numerator * frac2.Denominator - frac1.Denominator * frac2.Numerator, frac1.Denominator * frac2.Denominator);
+        }
+        static public Fraction MultSt(Fraction frac1, Fraction frac2)
+        {
+            return new Fraction(frac1.Numerator * frac2.Numerator, frac1.Denominator * frac2.Denominator);
+        }
+        static public Fraction DivSt(Fraction frac1, Fraction frac2)
+        {
+            return new Fraction(frac1.Numerator * frac2.Denominator, frac1.Denominator * frac2.Numerator);
+        }
+
+    }
+}
+/* Я умру хлебороб,- ел хурму я*/
